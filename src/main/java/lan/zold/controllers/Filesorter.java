69ab0ab809ad2fc3f2b74ef825b7c166431a79eb
Vito <lolozol80xd@gmail.com>
2023-11-09 09:56:31 +0100

    finished

--- src/main/java/lan/zold/controllers/Filesorter.java
+++ src/main/java/lan/zold/controllers/Filesorter.java
@@ -2,32 +2,59 @@ package lan.zold.controllers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import lan.zold.models.Organiser;
+
 public class Filesorter {
-    public void read(){
+    public ArrayList<Long> read() {
+        ArrayList<Long> numlist = null;
         try {
-            tryRead();
+            numlist = tryRead();
         } catch (FileNotFoundException e) {
             System.err.println("A file nem található.");
             System.err.println(e.getMessage());
         }
+        return numlist;
     }
-    public void tryRead() throws FileNotFoundException{
+
+    public ArrayList<Long> tryRead() throws FileNotFoundException {
+        ArrayList<Long> numlist = new ArrayList<>();
         File file = new File("C:/Users/diak/Vitovszki/asztali/genum/szamok.txt");
         Scanner sc = new Scanner(file);
+
         while (sc.hasNextLine()) {
-            System.out.println(sc.nextLine());
+            String numStr = sc.nextLine();
+            long num = Long.parseLong(numStr);
+            numlist.add(num);
         }
-
-
         sc.close();
-    }
-    public void sort(){
-    
-    }
-    public void write(){
-    
+        return numlist;
     }
 
+    public ArrayList<Long> sort() {
+        ArrayList<Long> sortedlist = new Organiser().bubbleSort(read());
+        return sortedlist;
     }
+
+    public void write (){
+        try {
+            tryWrite();
+        } catch (Exception e) {
+            System.err.println("Nemt található a file.");
+            System.err.println(e.getMessage());
+        }
+    }
+    public void tryWrite() throws IOException {
+        FileWriter fw = new FileWriter("sorted.txt");
+        for(Long num : sort()){
+            fw.write(num.toString());
+            fw.write("\n");
+        }
+        fw.close();
+    }
+
+}
