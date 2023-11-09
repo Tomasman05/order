bece33dc4540e858ea585f3877bdd3535562b979
Vito <lolozol80xd@gmail.com>
2023-11-09 09:04:05 +0100

    update

--- src/main/java/lan/zold/controllers/Filesorter.java
+++ src/main/java/lan/zold/controllers/Filesorter.java
@@ -1,14 +1,27 @@
 package lan.zold.controllers;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Filesorter {
-    public void tryRead() throws FileNotFoundException{
-        
-    }
-
     public void read(){
-    
+        try {
+            tryRead();
+        } catch (FileNotFoundException e) {
+            System.err.println("A file nem található.");
+            System.err.println(e.getMessage());
+        }
+    }
+    public void tryRead() throws FileNotFoundException{
+        File file = new File("C:/Users/diak/Vitovszki/asztali/genum/szamok.txt");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+
+        sc.close();
     }
     public void sort(){
     
