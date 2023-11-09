4e4cfc442b2fa9bc73319183806a312b93845377
Vito <lolozol80xd@gmail.com>
2023-11-09 08:08:35 +0100

    start

---
+++ src/main/java/lan/zold/models/Filesorter.java
@@ -0,0 +1,20 @@
+package lan.zold.models;
+
+import java.io.FileNotFoundException;
+
+public class Filesorter {
+    public void tryRead() throws FileNotFoundException{
+        
+    }
+
+    public void read(){
+    
+    }
+    public void sort(){
+    
+    }
+    public void write(){
+    
+    }
+
+    }
